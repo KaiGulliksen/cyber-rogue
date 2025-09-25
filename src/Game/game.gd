3dd402d534e8be9df1d0ed8dcdae18160d0f76cf
Kai Gulliksen <mail@kaigulliksen.com>
2025-09-25 21:50:26 +0200

    Tutorial Part 2 done

--- src/Game/game.gd
+++ src/Game/game.gd
@@ -2,10 +2,12 @@ class_name Game
 extends Node2D
 
 const player_definition: EntityDefinition = preload("res://src/Assets/Definitions/Entities/Actors/entity_definition_player.tres")
+const tile_size = 16
 
 @onready var player: Entity
 @onready var event_handler: EventHandler = $EventHandler
 @onready var entities: Node2D = $Entities
+@onready var map: Map = $Map
 
 
 func _ready() -> void:
@@ -13,5 +15,15 @@ func _ready() -> void:
 	player = Entity.new(player_start_pos, player_definition)
 	entities.add_child(player)
 	var npc := Entity.new(player_start_pos + Vector2i.RIGHT, player_definition)
-	npc.modulate = Color.GREEN
+	npc.modulate = Color.ORANGE_RED
 	entities.add_child(npc)
+
+
+func _physics_process(_delta: float) -> void:
+	var action: Action = event_handler.get_action()
+	if action:
+		action.perform(self, player)
+
+
+func get_map_data() -> MapData:
+	return map.map_data
