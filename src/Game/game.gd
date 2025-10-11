1d1e8dfc6cf97881807bca5437f124a7d6db9112
Kai Gulliksen <mail@kaigulliksen.com>
2025-10-11 10:58:37 +0200

    Player spawns in Hub

--- src/Game/game.gd
+++ src/Game/game.gd
@@ -1,7 +1,6 @@
 class_name Game
 extends Node2D
 
-
 signal player_created(player)
 
 const player_definition: EntityDefinition = preload("res://src/Assets/Definitions/Entities/Actors/entity_definition_player.tres")
@@ -9,7 +8,7 @@ const tile_size = 16
 
 @onready var player: Entity
 @onready var input_handler: InputHandler = $InputHandler
-@onready var map: Map = $Map
+@onready var hub: Hub = $Hub  # Changed from map to hub
 @onready var camera: Camera2D = $Camera2D
 
 
@@ -18,12 +17,13 @@ func _ready() -> void:
 	player_created.emit(player)
 	remove_child(camera)
 	player.add_child(camera)
-	map.generate(player)
-	map.update_fov(player.grid_position)
+	hub.generate(player)  # Changed from map.generate to hub.generate
+	update_fov(player.grid_position)
 	MessageLog.send_message.bind(
-		"Hello and welcome, adventurer, to yet another dungeon!",
+		"Hello and welcome, adventurer, to the hub!",
 		GameColors.WELCOME_TEXT
 	).call_deferred()
+
 
 func _physics_process(_delta: float) -> void:
 	var action: Action = input_handler.get_action(player)
@@ -31,7 +31,7 @@ func _physics_process(_delta: float) -> void:
 		var previous_player_position: Vector2i = player.grid_position
 		action.perform()
 		_handle_enemy_turns()
-		map.update_fov(player.grid_position)
+		update_fov(player.grid_position)
 
 
 func _handle_enemy_turns() -> void:
@@ -41,4 +41,11 @@ func _handle_enemy_turns() -> void:
 
 
 func get_map_data() -> MapData:
-	return map.map_data
+	return hub.map_data  # Changed from map.map_data
+
+
+func update_fov(player_position: Vector2i) -> void:
+	# You'll need to add a FieldOfView node to the Hub or reuse the Map's one
+	# For now, we can create a simple version
+	for entity in get_map_data().entities:
+		entity.visible = true  # Make all entities visible in the hub for now
--- src/Map/hub.gd
+++ src/Map/hub.gd
@@ -1,10 +1,56 @@
 class_name Hub
 extends Node2D
 
-var map_data = MapData
-
 @export var hub_width: int = 74
 @export var hub_height: int = 74  
 @export var player_spawn_position: Vector2i = Vector2i(35, 20)
 
+var map_data: MapData
+var tile_map: Tile
+
 @onready var tile_map_layer: TileMapLayer = $TileMapLayer
+@onready var tiles: Node2D = $Tiles
+@onready var entities: Node2D = $Entities
+
+
+func generate(player: Entity) -> void:
+	map_data = MapData.new(hub_width, hub_height, player)
+	_setup_tiles_from_tilemap()
+	
+	player.grid_position = player_spawn_position
+	player.map_data = map_data
+	map_data.entities.append(player)
+	entities.add_child(player)
+	
+	map_data.setup_pathfinding()
+
+
+func _setup_tiles_from_tilemap() -> void:
+	for y in range(hub_height):
+		for x in range(hub_width):
+			var tile_pos = Vector2i(x, y)
+			var tile: Tile = map_data.get_tile(tile_pos)
+			
+			var atlas_coords = tile_map_layer.get_cell_atlas_coords(tile_pos)
+			
+			if atlas_coords == Vector2i(-1, -1):
+				# No tile - default to wall
+				tiles.add_child(tile)
+				continue
+			
+			var tile_data: TileData = tile_map_layer.get_cell_tile_data(tile_pos)
+			if tile_data:
+				# Read multiple custom data properties
+				var is_walkable = tile_data.get_custom_data("is_walkable") if tile_data.has_custom_data("is_walkable") else false
+				var is_transparent = tile_data.get_custom_data("is_transparent") if tile_data.has_custom_data("is_transparent") else is_walkable
+				
+				# Choose tile type based on walkability
+				if is_walkable:
+					tile.set_tile_type(TileDB.TileName.FLOOR1)
+				else:
+					tile.set_tile_type(TileDB.TileName.WALL1)
+				
+				# You could also manually override transparency if you added that custom data
+				# tile._definition.is_transparent = is_transparent
+			
+			tiles.add_child(tile)
